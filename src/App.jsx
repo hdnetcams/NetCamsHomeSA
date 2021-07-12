186c167ffe8d0e66bded7997455e7d10b1a1b541
zpetukhov <zpetukhov@gmail.com>
2021-07-13 01:35:26 +0300

    Unknown URI's processing added to the Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import NavbarState from './components/Navbar/NavbarContext/NavbarState';
 import HomePage from './components/Home/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <NavbarState>
-        <HomePage />
-      </NavbarState>
+      <Switch>
+        <Route exact path="/" render={ () =>
+          <NavbarState>
+            <HomePage />
+          </NavbarState>
+        } />
+        <Route render={ (props) =>
+          <div style={{ padding: '10px' }}>
+            Unknown URI: <br />
+            { props.location.pathname } <br />
+            <a style={{ color: 'blue' }} href={'https://netcams.io' + props.location.pathname}>Click here to open this link on netcams.io website</a> <br />
+            Or use "back" browser button to return to the app.
+            { /* { JSON.stringify(props) } */ }
+          </div>
+        } />
+      </Switch>
     </Router>
   );
 }
